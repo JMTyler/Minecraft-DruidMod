693f79ffbcd2dcc0a7384c723f34ddf7d3c6186c
Jared Tyler <jtyler@wdca.ca>
2012-03-02 01:47:03 -0500

    A wolf familiar will now spawn when druid puts on helmet, despawn when helmet is removed. Also, you can craft healing water, but it's weird and broken at the moment.

--- link-druid.bat
+++ link-druid.bat
@@ -7,15 +7,25 @@ set dest_path=".\src\minecraft\net\minecraft\src"
 
 
 REM remove old links or files
+del /s /q  %dest_path%\BlockHealingWaterFlowing.java
+del /s /q  %dest_path%\BlockHealingWaterStationary.java
 del /s /q  %dest_path%\EntityLiving.java
+del /s /q  %dest_path%\EntityWolfFamiliar.java
+del /s /q  %dest_path%\ItemBucket.java
 del /s /q  %dest_path%\ItemHealingBark.java
+del /s /q  %dest_path%\ItemHealingWater.java
 del /s /q  %dest_path%\ItemMagicLeaf.java
 del /s /q  %dest_path%\mod_Druid.java
 
 
 REM rebuild links
+mklink	%dest_path%\BlockHealingWaterFlowing.java		%source_path%\BlockHealingWaterFlowing.java
+mklink	%dest_path%\BlockHealingWaterStationary.java	%source_path%\BlockHealingWaterStationary.java
 mklink	%dest_path%\EntityLiving.java			%source_path%\EntityLiving.java
+mklink	%dest_path%\EntityWolfFamiliar.java		%source_path%\EntityWolfFamiliar.java
+mklink	%dest_path%\ItemBucket.java				%source_path%\ItemBucket.java
 mklink	%dest_path%\ItemHealingBark.java		%source_path%\ItemHealingBark.java
+mklink	%dest_path%\ItemHealingWater.java		%source_path%\ItemHealingWater.java
 mklink	%dest_path%\ItemMagicLeaf.java			%source_path%\ItemMagicLeaf.java
 mklink	%dest_path%\mod_Druid.java				%source_path%\mod_Druid.java
 
--- mod_Druid.java
+++ mod_Druid.java
@@ -17,8 +17,12 @@ public class mod_Druid extends BaseMod
 	
 	public static final String armorGroup = "druid";
 	
+	public static Block blockHealingWaterMoving;
+	public static Block blockHealingWaterStill;
+	
 	public static Item healingBark;
 	public static Item magicLeaf;
+	public static Item itemHealingWater;
 	
 	public static Item druidHelmet;
 	public static Item druidBracers;
@@ -29,6 +33,9 @@ public class mod_Druid extends BaseMod
 	public static int _leafGrowthState = 0;
 	public static int _leafGrowthType = LEAF_GROWTH_NONE;
 	
+	protected boolean _hasWolfSpawned = false;
+	protected EntityWolfFamiliar _familiar = null;
+	
 	
 	static
 	{
@@ -38,20 +45,32 @@ public class mod_Druid extends BaseMod
 			druidPantaloons = mod_Classes.addArmor("druidPantaloons", "Natural Pantaloons", new ItemClassArmor(mod_Classes.getUniqueItemId(), armorGroup, EnumArmorType.Legs, 0));
 			druidBooties = mod_Classes.addArmor("druidBooties", "Le Bare Feet", new ItemClassArmor(mod_Classes.getUniqueItemId(), armorGroup, EnumArmorType.Boots, 0));
 			
-			if (druidHelmet != null) {
+			if (druidPantaloons != null) {
+				blockHealingWaterMoving = new BlockHealingWaterFlowing();
+				blockHealingWaterStill = new BlockHealingWaterStationary();
+				
 				healingBark = mod_Classes.addWeapon("healingBark", "Healing Bark", new ItemHealingBark()).dependsOn((ItemClassArmor)druidPantaloons);
 				magicLeaf = mod_Classes.addWeapon("magicLeaf", "Magical Leaf", new ItemMagicLeaf()).dependsOn((ItemClassArmor)druidPantaloons);
+				itemHealingWater = new ItemHealingWater(); // TODO: should be able to integrate something like this into mod_Classes
 			}
 		}
 	}
 	
 	public mod_Druid()
 	{
+		/* Setting up blocks */
+		ModLoader.RegisterBlock(blockHealingWaterMoving);
+		ModLoader.RegisterBlock(blockHealingWaterStill);
+		
+		
 		/* Setting up item icons */
 		healingBark.iconIndex = ModLoader.addOverride("/gui/items.png", "/gui/druidBark.png");
 		magicLeaf.iconIndex = ModLoader.addOverride("/gui/items.png", "/gui/druidLeaf.png");
+		itemHealingWater.iconIndex = ModLoader.addOverride("/gui/items.png", "/gui/healingWaterBucket.png");
 		druidHelmet.iconIndex = ModLoader.addOverride("/gui/items.png", "/gui/druidHelmet.png");
+		druidBracers.iconIndex = ModLoader.addOverride("/gui/items.png", "/gui/druidBracers.png");
 		druidPantaloons.iconIndex = ModLoader.addOverride("/gui/items.png", "/gui/druidLeggings.png");
+		druidBooties.iconIndex = ModLoader.addOverride("/gui/items.png", "/gui/druidBooties.png");
 		
 		Item testItem = new Item(mod_Classes.getUniqueItemId()) {
 			@Override
@@ -66,13 +85,31 @@ public class mod_Druid extends BaseMod
 		ModLoader.AddName(testItem, "test");
 		
 		
+		/* Setting up recipes */
+		ModLoader.AddRecipe(new ItemStack(itemHealingWater, 1), new Object[] {
+			"X", "Y", Character.valueOf('X'), healingBark, Character.valueOf('Y'), Item.bucketWater
+		});
+		
+		
+		/* Setting up entities */
+		ModLoader.RegisterEntityID(EntityWolfFamiliar.class, "WolfFamiliar", mod_Classes.getUniqueEntityId());
+		
+		
 		ModLoader.SetInGameHook(this, true, true);
+	}
+	
+	@Override
+	public void AddRenderer(Map map)
+	{
+		map.put(EntityWolfFamiliar.class, new RenderWolf(new ModelWolf(), 0.5F));
 	}
 	
 	@Override
 	public boolean OnTickInGame(float f, Minecraft minecraft)
 	{
 		super.OnTickInGame(f, minecraft);
+		
+		EntityPlayer player = minecraft.thePlayer;
 		
 		if (_leafGrowthState > 0) {
 			World world = minecraft.theWorld;
@@ -94,7 +131,48 @@ public class mod_Druid extends BaseMod
 			}
 		}
 		
+		boolean wearingHelmet = mod_Classes.checkDependency(minecraft.thePlayer, mod_Classes.getArmor("druidHelmet"));
+		if (!_hasWolfSpawned && wearingHelmet) {
+			boolean isSpawned = _spawnWolfFamiliar(minecraft.theWorld, player);
+			_hasWolfSpawned = true;
+		} else if (_hasWolfSpawned && !wearingHelmet) {
+			if (_familiar != null) minecraft.theWorld.setEntityDead(_familiar);
+			_hasWolfSpawned = false;
+		}
+		
 		return true;
+	}
+	
+	protected boolean _spawnWolfFamiliar(World world, EntityPlayer player)
+	{
+		EntityLiving entityliving = (EntityLiving)EntityList.createEntityInWorld("WolfFamiliar", world);
+        if(entityliving == null)
+        {
+            return false;
+        }
+        double d6 = (double)player.posX + (world.rand.nextDouble() - world.rand.nextDouble()) * 4D;
+        double d7 = (player.posY + world.rand.nextInt(3)) - 1;
+        double d8 = (double)player.posZ + (world.rand.nextDouble() - world.rand.nextDouble()) * 4D;
+        entityliving.setLocationAndAngles(d6, d7, d8, world.rand.nextFloat() * 360F, 0.0F);
+//        if(!entityliving.getCanSpawnHere())
+//        {
+//            return false;
+//        }
+        world.entityJoinedWorld(entityliving);
+        for(int k = 0; k < 20; k++)
+        {
+//            double d1 = (double)xCoord + 0.5D + ((double)worldObj.rand.nextFloat() - 0.5D) * 2D;
+//            double d3 = (double)yCoord + 0.5D + ((double)worldObj.rand.nextFloat() - 0.5D) * 2D;
+//            double d5 = (double)zCoord + 0.5D + ((double)worldObj.rand.nextFloat() - 0.5D) * 2D;
+//            worldObj.spawnParticle("smoke", d1, d3, d5, 0.0D, 0.0D, 0.0D);
+        }
+
+        entityliving.spawnExplosionParticle();
+        
+        _familiar = (EntityWolfFamiliar)entityliving;
+        _familiar.setOwner(player.username);
+        
+        return true;
 	}
 	
 	@Override
